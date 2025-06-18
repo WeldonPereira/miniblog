e67c783f1e6ddf7878194809b84699281839af38
Weldon <weldonbarros179@gmail.com>
2025-06-18 10:10:51 -0300

    style: adicionar estilo no navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,16 +3,37 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <div>
-      <nav>
-        <NavLink to="/">
-          Mini <span>Blog</span>
+      <nav
+        style={{ boxShadow: "0 -2px 10px 0 rgba(0, 0, 0, 0.15)" }}
+        className="flex items-center justify-between py-2 px-8"
+      >
+        <NavLink to="/" className="text-xl">
+          Mini <span className="font-black uppercase">Blog</span>
         </NavLink>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
+        <ul className="flex">
+          <li className="mx-4">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "py-2 px-3 font-black text-xl"
+                  : "py-2 px-3 text-xl"
+              }
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/about">Sobre</NavLink>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive
+                  ? "py-2 px-3 font-black text-xl"
+                  : "py-2 px-3 text-xl"
+              }
+            >
+              Sobre
+            </NavLink>
           </li>
         </ul>
       </nav>
