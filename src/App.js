2bc07ef580727256f7df82651a1fc079159accbf
Weldon <weldonbarros179@gmail.com>
2025-06-23 09:00:32 -0300

    feat: passar usuário para o contexto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
@@ -7,11 +8,28 @@ import Register from "./pages/Register/Register";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import { AuthProvider } from "./context/AuthContext";
+import { useAuthentication } from "./hooks/useAuthentication";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    <p>Carregando...</p>;
+  }
+
   return (
     <div>
-      <AuthProvider>
+      <AuthProvider value={{ user }}>
         <BrowserRouter>
           <NavBar />
           <div className="container">
