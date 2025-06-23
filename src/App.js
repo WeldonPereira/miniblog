fef089b41a50612abf05f598d71b5534f32ddbec
Weldon <weldonbarros179@gmail.com>
2025-06-23 08:46:24 -0300

    feat: criar contexto

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <NavBar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <NavBar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
